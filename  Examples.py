7501a99348f6f9ccadfbe2ad31c8e2a1813abed4
“[my <[my>
2022-11-28 15:24:05 +0530

    deleted some lines

---  Examples.py
+++  Examples.py
@@ -1,27 +1,8 @@
 ## Examples
 
-### Styling a paragraph
-
 In this example, we're using CSS to style paragraphs that have the `fancy` class set.
 
-#### HTML
-
-```html
-<p>I'm not fancy.</p>
-
-<p class="fancy">But I am!</p>
-```
-
-#### CSS
-
-```css
-p.fancy {
-  color: red;
-}
-```
-
-#### Result
 
 {{EmbedLiveSample("Styling a paragraph")}}
 
-Only the `<p>` element with `class="fancy"` will get styled `red`.
+Only the `<p>` element with `class="fancy"` my is sridhar i am from hyd `red`.
